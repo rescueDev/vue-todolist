7f731902f2388fbe615c496e2f6c2abd19cd9652
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-18 14:02:28 +0100

    base aggiunta e rimozione ultimo task

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@
 // un input permette all’utente di scrivere una “cosa da fare”;
 // un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
 // una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
+
+//creo istanza Vue
+
+var app = new Vue({
+  el: "#root",
+  data: {
+    dato: " ",
+    tasks: [],
+    datoIndex: 0,
+  },
+  methods: {
+    addTasks() {
+      this.tasks.push(this.dato);
+    },
+    removeTasks() {
+      this.tasks.pop(this.dato);
+    },
+  },
+});
