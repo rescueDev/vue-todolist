4720758a4442d09ed7e2048d9f03b5f7da30f016
Salvatore Borgia <sa.borgia3@gmail.com>
2020-11-18 11:58:21 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// DESCRIZIONE:
+// Creiamo una mini app di todolist con html+css+js in Vue
+// Features principali:
+// un input permette all’utente di scrivere una “cosa da fare”;
+// un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
+// una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
